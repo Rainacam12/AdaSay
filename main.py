c232344fd52281413d6b37a222071e0d7f3a2c46
Winslow <owedwards@gmail.com>
2023-03-23 10:30:47 -0400

    Turned cow to dragon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import cowsay
 
-cowsay.cow("Somehow I'm a cow")
-cowsay.cow("B*tch I'm a cow, I'm not a cat, I don't say meow!")
+cowsay.dragon("Somehow I'm a cow")
+cowsay.dragon("B*tch I'm a cow, I'm not a cat, I don't say meow!")
